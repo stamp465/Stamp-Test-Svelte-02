08dd67a46b841a144cacb550378d97c4ef980596
Witnapat <lovelove6985@gmail.com>
2022-05-17 11:15:35 +0700

    Add graphql ,api key and get post data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="js">
+	import { operationStore, query, setClient } from '@urql/svelte';
+	import client from '../client';
+	setClient(client);
+
+	const allPosts = operationStore(
+		`
+    query GetAllPosts($size: Int!, $cursor: String) {
+      listPosts(_size: $size, _cursor: $cursor) {
+        data {
+          _id
+          title
+          author {
+            email
+          }
+        }
+      }
+    }
+  `,
+		{ size: 100 },
+		{ requestPolicy: 'network-only' }
+	);
+
+	query(allPosts);
+
+</script>
+
+<h1>Posts</h1>
+
+{#if $allPosts.fetching}
+	<p>Loading...</p>
+{:else if $allPosts.error}
+	<p>Oh no... {$allPosts.error.message}</p>
+{:else}
+	{#each $allPosts.data.listPosts.data as post}
+		<div class="post-wrap">
+			<a href="/posts/${post._id}">
+				<div>{post.title}</div>
+			</a>
+			<span>by {post.author.email}</span>
+		</div>
+	{/each}
+{/if}
+
+<style>
+	.post-wrap {
+		margin-bottom: 1rem;
+	}
+</style>
