0ffebe2e8b3e88d072ca2c4f1c3255e0001bd8d8
Witnapat <lovelove6985@gmail.com>
2022-05-18 09:29:07 +0700

    Add edit and Delete

--- src/client.js
+++ src/client.js
@@ -1,16 +1,27 @@
-import { createClient } from '@urql/svelte'
+import { createClient } from '@urql/svelte';
 
 export default createClient({
-  url: 'https://graphql.us.fauna.com/graphql',
+	url: 'https://graphql.us.fauna.com/graphql',
 
-  // For DB in other zone use the following url
-    // EU: https://graphql.eu.fauna.com/graphql
-  // Classic: https://graphql.fauna.com/graphql
+	// For DB in other zone use the following url
+	// EU: https://graphql.eu.fauna.com/graphql
+	// Classic: https://graphql.fauna.com/graphql
 
-  fetchOptions: () => {
-    const token = import.meta.env.VITE_PUBLIC_FAUNA_KEY;
-    return {
-      headers: { authorization: token ? `Bearer ${token}` : '' },
-    }
-  },
-})
+	fetchOptions: () => {
+		const token = import.meta.env.VITE_PUBLIC_FAUNA_KEY;
+		return {
+			headers: { authorization: token ? `Bearer ${token}` : '' }
+		};
+	}
+});
+
+export const clientWithAuthToken = (token) =>
+	createClient({
+		url: 'https://graphql.us.fauna.com/graphql',
+		fetchOptions: () => {
+			console.log('token', token);
+			return {
+				headers: { authorization: token ? `Bearer ${token}` : '' }
+			};
+		}
+	});
--- src/routes/posts/[id].svelte
+++ src/routes/posts/[id].svelte
@@ -2,6 +2,8 @@
 	import { operationStore, query, setClient } from '@urql/svelte';
 	import { page } from '$app/stores';
 	import client from '../../client';
+	import Delete from '$lib/Delete.svelte';
+	import Edit from '$lib/Edit.svelte';
 	setClient(client);
 
 	const currentPost = operationStore(
@@ -21,6 +23,14 @@
 	);
 
 	query(currentPost);
+
+	export let post = null;
+
+	currentPost.subscribe(({ data }) => {
+		if (data) {
+			post = data.findPostByID;
+		}
+	});
 </script>
 
 {#if $currentPost.fetching}
@@ -29,4 +39,6 @@
 	<h2>{$currentPost.data.findPostByID.title}</h2>
 	<p>By <b>{currentPost.data.findPostByID.author.email}</b></p>
 	<p>{$currentPost.data.findPostByID.content}</p>
+	<Edit {post} />
+	<Delete />
 {/if}
